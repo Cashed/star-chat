b7eebab91b8589684ad0ca4ed30cce62eadc37fd
Veronica Moreno <frau.veronika@gmail.com>
2016-03-08 13:59:41 -0800

    profile pics assigned per user on signin

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 (function() {
   $(document).ready(function() {
     var chatID;
-    var pic = 'media/biking.jpg';
+    var profilePic;
     var messageRef = new Firebase('https://star-chat.firebaseio.com/');
+    var pics = ['media/profilePics/709.jpg', 'media/profilePics/archer.jpg', 'media/profilePics/cardassian.jpg', 'media/profilePics/data.jpg', 'media/profilePics/deanna.jpg', 'media/profilePics/doctor.jpg', 'media/profilePics/ferengi.jpg', 'media/profilePics/geordi.jpg', 'media/profilePics/janeway.jpg', 'media/profilePics/locutus.jpg', 'media/profilePics/neelix.jpg', 'media/profilePics/phlox.jpg', 'media/profilePics/picard.jpg', 'media/profilePics/q.jpg', 'media/profilePics/riker.jpg', 'media/profilePics/tpol.jpg', 'media/profilePics/wesley.jpg', 'media/profilePics/worf.jpg'];
+
     $('#chatID').keypress(function(e) {
       if(e.keyCode === 13) {
         chatID = $('#chatID').val();
+        profilePic = pics[Math.floor(Math.random() * 18)];
         $('.login').hide();
       }
     });
@@ -16,7 +19,7 @@
 
       message.keypress(function(e) {
         if(e.keyCode === 13) {
-          messageRef.push({name:chatID, text:message.val()});
+          messageRef.push({name:chatID, text:message.val(), pic:profilePic});
           message.val('');
         }
       });
@@ -28,13 +31,18 @@
       var board = $('#board');
       var messagePost = $('<div id="messagePost">');
       var name = $('<a>');
+      var picture = $('<img>');
       var data = snapshot.val();
       var message = data.text;
+
       name.text(data.name);
       name.attr('href', 'http://google.com');
 
+      picture.attr('src', data.pic);
+
       messagePost.text(': ' + message);
       messagePost.prepend(name);
+      messagePost.prepend(picture);
 
       board.append(messagePost);
 
